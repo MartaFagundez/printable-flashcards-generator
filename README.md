4f5a6339e5e75cb11abf5ce880e833e93aa78eeb
MartaFagundez <55920614+MartaFagundez@users.noreply.github.com>
2024-03-19 15:33:41 -0300

    refactor: improve Readme and info in modal

--- README.md
+++ README.md
@@ -1,8 +1,49 @@
-# Printable Flashcards App
+![Printable Flashcards Generator screenshot](/public/assets/home-image.jpg)
+
+# Printable Flashcards Generator
 
 This is a React application designed to generate flashcards optimized for printing on both sides. Each flashcard contains a question on the front and its corresponding answer on the back.
 
-## Usage
+## How It Works
+
+### File Format
+
+The data must be contained in a .json file with the following structure:
+
+```json
+[
+    {
+      "id": "string (optional, max length: 6)",
+      "question": "string (required, max length: 120)",
+      "answer": "string (required, max length: 250)",
+      "category_name": "string (optional, max length: 25)",
+      "qr_url": "string (optional, URL pointing to .jpg or .png image)"
+    },
+    ...
+]
+```
+
+The file must contain at least 1 flashcard and can have a maximum of 120 flashcards.
+
+#### Field definitions
+
+- **id:** Optional. Unique identifier for the flashcard. Accepts string values with a maximum length of 6 characters.
+- **question:** <mark>Required.</mark> The question for the flashcard. Accepts string values with a maximum length of 120 characters.
+- **answer:** <mark>Required.</mark> The answer to the question. Accepts string values with a maximum length of 250 characters.
+- **category_name:** Optional. Name of the category to which the flashcard belongs. Accepts string values with a maximum length of 25 characters.
+- **qr_url:** Optional. URL pointing to a .jpg or .png image for a QR code.
+
+### Print Settings
+
+First you generate a pdf file and then print it.
+In [this image](/public/assets/save_as_pdf_settings.png) you can see the configuration required to generate the pdf file correctly.
+
+### Example Files
+
+- [File to generate the flashcards](/public/assets/example.json) (.json)
+- [File generated and ready to print](/public/assets/example.pdf) (.pdf)
+
+## Development
 
 ### Setup
 
@@ -13,13 +54,11 @@ This is a React application designed to generate flashcards optimized for printi
    npm install
    ```
 
-## Development
+3. Start the development server by running:
 
-This project was developed using Vite. You can start the development server by running:
-
-```bash
-npm run dev
-```
+   ```bash
+   npm run dev
+   ```
 
 ## License
 
--- src/components/Faqs.jsx
+++ src/components/Faqs.jsx
@@ -6,7 +6,7 @@ const code = `[
     {
       "id": "string (optional, max length: 6)",
       "question": "string (required, max length: 120)",
-      "answer": "string (required, max length: 240)",
+      "answer": "string (required, max length: 250)",
       "category_name": "string (optional, max length: 25)",
       "qr_url": "string (optional URL pointing to .jpg or .png image)"
     },
@@ -35,7 +35,7 @@ export default function Faqs() {
                 <path d="M10.854 7.854a.5.5 0 0 0-.708-.708L7.5 9.793 6.354 8.646a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0z"/>
                 <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2M9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5z"/>
             </svg>
-            File format
+            File Format
           </button>
         </h2>
         <div
@@ -45,26 +45,19 @@ export default function Faqs() {
         >
           <div className="accordion-body">
             <div className="accordion-body-block">
-                <p>The JSON file must adhere to the following structure:</p>
+                <p>The data must be contained in a .json file with the following structure:</p>
                 <CodeBlock code={code} language={"json"} />
+                <p>The file must contain at least 1 flashcard and can have a maximum of 120 flashcards.</p>
             </div>
 
             <div className="accordion-body-block">
                 <h4>Field definitions:</h4>
                 <ul>
-                    <li><span>id: </span>Unique identifier for the flashcard. <mark>Optional.</mark> Accepts string values with a maximum length of 6 characters.</li>
-                    <li><span>question: </span>The question for the flashcard. <mark>Required.</mark> Accepts string values with a maximum length of 120 characters.</li>
-                    <li><span>answer: </span>The answer to the question. <mark>Required.</mark> Accepts string values with a maximum length of 240 characters.</li>
-                    <li><span>category_name: </span>Name of the category to which the flashcard belongs. <mark>Optional.</mark> Accepts string values with a maximum length of 25 characters.</li>
-                    <li><span>qr_url: </span>URL pointing to a .jpg or .png image for a QR code. <mark>Optional.</mark></li>
-                </ul>
-            </div>
-
-            <div className="accordion-body-block">
-                <h4>Additional Information:</h4>
-                <ul>
-                    <li>The JSON file must contain at least 1 flashcard and can have a maximum of 120 flashcards.</li>
-                    <li>The URL provided in qr_url field, if present, should point to an image file with the extension .jpg or .png.</li>
+                    <li><span>id: </span>Optional. Unique identifier for the flashcard. Accepts string values with a maximum length of 6 characters.</li>
+                    <li><span>question: </span><mark>Required.</mark>The question for the flashcard. Accepts string values with a maximum length of 120 characters.</li>
+                    <li><span>answer: </span><mark>Required.</mark> The answer to the question. Accepts string values with a maximum length of 250 characters.</li>
+                    <li><span>category_name: </span>Optional. Name of the category to which the flashcard belongs. Accepts string values with a maximum length of 25 characters.</li>
+                    <li><span>qr_url: </span>Optional. URL pointing to a .jpg or .png image for a QR code.</li>
                 </ul>
             </div>
           </div>
@@ -96,7 +89,7 @@ export default function Faqs() {
         >
           <div className="accordion-body">
             <p>First you generate a pdf file and then print it.</p>
-            <p>In <a href={`${assetsUrlBase}save_as_pdf_settings.png`} target="_blank" rel="noopener noreferrer">this image </a> you can see the configuration required to generate the file correctly.</p>
+            <p>In <a href={`${assetsUrlBase}save_as_pdf_settings.png`} target="_blank" rel="noopener noreferrer">this image </a> you can see the configuration required to generate the pdf file correctly.</p>
           </div>
         </div>
       </div>
@@ -116,7 +109,7 @@ export default function Faqs() {
                 <path d="M8.5 6.5a.5.5 0 0 0-1 0v3.793L6.354 9.146a.5.5 0 1 0-.708.708l2 2a.5.5 0 0 0 .708 0l2-2a.5.5 0 0 0-.708-.708L8.5 10.293z"/>
                 <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2M9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5z"/>
             </svg>
-            Example files
+            Example Files
           </button>
         </h2>
         <div
